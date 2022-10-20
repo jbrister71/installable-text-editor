d69daf8a926a8aa802b34ed3ff5004a3f9030749
Bell <jacobbrister71@gmail.com>
2022-10-20 02:25:40 -0400

    added ability to save a retrieve from indexeddb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,7 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime'
 
-const initdb = async () =>
+export const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -13,9 +14,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Updating Data...');
+
+  const jateDB = await openDB('jate', 1);
+
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ content: content });
+
+  const result = await request;
+  console.log('Data updated', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Retrieving Data...');
+
+  const jateDB = await openDB('jate', 1);
+
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result[result.length - 1].content;
+}
 
 initdb();
