d2e73b3234ebd17f98632e494e6522a83893176e
Bell <jacobbrister71@gmail.com>
2022-10-20 22:45:43 -0400

    added ability to install app

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
           type: 'asset/resource',
         },
         {
